49c268a0deeaaa4bb1f69b44b232c51ad463d482
martinpare1208 <martinapilado@gmail.com>
2024-07-23 09:51:00 -0400

    implemented API get request

--- responses.py
+++ responses.py
@@ -1,25 +1,36 @@
 import random
 from discord import Intents, Client, Message, Embed
 from re import findall as findall
-TRUCKING_FACTS = [
-    
-]
+import os
+from dotenv import load_dotenv
+from typing import Final
+import requests
+from datetime import date
+
+#Load Stock Data API key
+load_dotenv()
+TOKEN: Final[str] = os.getenv('STOCK_DATA_API_KEY')
+
+#URL for stock data
+URL = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey={TOKEN}'
 
 def get_response(user_input: str) -> Embed:
     lowered: str = user_input.lower()
-    response_embed = Embed(title=lowered)
     
-    if 'stock' in user_input:
-        stock_input = findall(r'\w+', user_input[6:])
-        print(stock_input)
-        response_embed = get_stock_data()
+    if 'stock' in lowered:
+        stock_input = findall(r'\w+', lowered[6:])
+        response_embed = get_stock_data(stock_input)
         return response_embed
 
-    return response_embed
+    return
 
 
-def get_stock_data() -> Embed:
-
+def get_stock_data(user_input: str) -> Embed:
+    r = requests.get(URL)
+    data = r.json()
+    today = date.today()
+    date_as_str = today.strftime(r"%Y-%m-%d")
+    print(date_as_str)
     return Embed(title="STOCK DATA!!!!!")
    
 
