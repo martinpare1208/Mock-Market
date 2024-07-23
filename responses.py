4e1ef733bb5bcfd2a3075267d50d32a4a64f9b01
martinpare1208 <martinapilado@gmail.com>
2024-07-23 10:57:18 -0400

    Access yesterday's closing price

--- responses.py
+++ responses.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 from typing import Final
 import requests
-from datetime import date
+from datetime import date, timedelta
 
 #Load Stock Data API key
 load_dotenv()
@@ -19,19 +19,20 @@ def get_response(user_input: str) -> Embed:
     
     if 'stock' in lowered:
         stock_input = findall(r'\w+', lowered[6:])
-        response_embed = get_stock_data(stock_input)
+        embed_data = get_stock_data()
+        response_embed = Embed(title=embed_data[0])
         return response_embed
 
     return
 
 
-def get_stock_data(user_input: str) -> Embed:
+def get_stock_data() -> list:
     r = requests.get(URL)
     data = r.json()
-    today = date.today()
+    today = date.today() - timedelta(days = 1)
     date_as_str = today.strftime(r"%Y-%m-%d")
-    print(date_as_str)
-    return Embed(title="STOCK DATA!!!!!")
+    closing_price = data["Time Series (Daily)"][date_as_str]['4. close']
+    return list([closing_price, date_as_str])
    
 
 
