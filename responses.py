fd42574e6f410de36f1cbceb209b33663147455a
martinpare1208 <martinapilado@gmail.com>
2024-07-23 09:08:51 -0400

    implemented regex to get stock data from user input

--- responses.py
+++ responses.py
@@ -1,5 +1,6 @@
 import random
 from discord import Intents, Client, Message, Embed
+from re import findall as findall
 TRUCKING_FACTS = [
     
 ]
@@ -7,8 +8,10 @@ TRUCKING_FACTS = [
 def get_response(user_input: str) -> Embed:
     lowered: str = user_input.lower()
     response_embed = Embed(title=lowered)
-
+    
     if 'stock' in user_input:
+        stock_input = findall(r'\w+', user_input[6:])
+        print(stock_input)
         response_embed = get_stock_data()
         return response_embed
 
@@ -16,7 +19,7 @@ def get_response(user_input: str) -> Embed:
 
 
 def get_stock_data() -> Embed:
-    
+
     return Embed(title="STOCK DATA!!!!!")
    
 
