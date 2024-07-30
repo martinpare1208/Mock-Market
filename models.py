0559bc6a0b3778a3d0a367bb87ccc0ea192bf56e
martinpare1208 <martinapilado@gmail.com>
2024-07-30 11:27:52 -0400

    created engine connection to db

--- models.py
+++ models.py
@@ -1,14 +1,19 @@
 import sqlalchemy as db
 import sqlalchemy.orm as orm
+from main import db_path
+
+
 
 class Base(db.DeclarativeBase):
     pass
 
 
 class BankAccount(db.Base):
-    __tablename__ = 'bank_accounts'
+    __tablename__ = 'bankAccount'
 
     id = orm.mapped_column(db.Integer, primary_key = True)
     money = orm.mapped_column(db.Float, nullable = False, default=0)
     username = orm.mapped_column(db.String(50), nullable = False, unique=True)
+    
+    
     
